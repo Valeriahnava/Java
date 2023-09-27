c64473e14d410854784d59ebf980fb15ccf82dcd
edc5 <edc5@test.com>
2023-09-27 14:39:17 -0600

    Operadores de Asignación

---
+++ src/Deasignacion.java
@@ -0,0 +1,14 @@
+public class Deasignacion {
+
+    public static void operadoresDeAsignacion(){
+        int a = 13;
+        System.out.println(a);
+    }
+    
+    public static void operadorsumaigual(){
+        int a = 5;
+        a+=5;
+
+    
+    }
+}
