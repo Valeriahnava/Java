5e969c68e052f62a254196914909803865a66c51
edc5 <edc5@test.com>
2023-11-13 19:17:48 -0600

    Entregables completos

--- Entregable7/src/App.java
+++ Entregable7/src/App.java
@@ -1,9 +1,19 @@
 
 
-public class App {
-    public static void main(String[] args) throws Exception {
-        String[] arreglo;
-        StringArray = new String[5];
-        System.out.println("Perros actuales en la estética: ");
+    public class App {
+    public static void main(String[] args) {
+
+        RecibeMascota[] esteticadeperros = new RecibeMascota[3];
+
+        RecibeMascota Mascota1 = new RecibeMascota("sam", 5, "husky", 10.0, "erick");
+        RecibeMascota Mascota2 = new RecibeMascota("lili", 6, "akita", 15.0, "yaz");
+        RecibeMascota Mascota3 = new RecibeMascota("drako", 3, "pitbull", 13.0, "claudio");
+
+        esteticadeperros[0] = Mascota1;
+        esteticadeperros[1] = Mascota2;
+        esteticadeperros[2] = Mascota3;
+
+        System.out.println("perros actuales en la estetica: " + esteticadeperros.length);
     }
 }
+
--- Entregable8/src/App.java
+++ Entregable8/src/App.java
@@ -3,16 +3,20 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
-        Scanner datos = new Scanner;
-        String Nombre;
-        Int Edad;
+        String nombre;
+        int edad;
+        System.out.println("ingresa tu nombre: ");
+        nombre = sc.nextLine();
+        System.out.println("ingresa tu edad: ");
+        edad = sc.nextInt();
 
-        System.out.println("Ingresa tu nombre completo: ");
-        System.out.println("Ingresa tu edad");
-        int edad = sc.nextInt();
-        if (edad>18) {System.out.println("Eres mayor de edad");
-            
+        if (edad >= 18) {
+            System.out.println("Tu nombre es " + nombre + ", tienes " + edad + " años y eres mayor de edad.");
+        } else {
+            System.out.println("Tu nombre es " + nombre + ", tienes " + edad + " años y eres menor de edad.");
         }
 
+        System.out.println("Tu nombre tiene " + nombre.length() + " caracteres.");
     }
 }
+
